317fa731e50a5f681631b6ff56c071613e8adf9d
tkitada <taka.lebron.2306@gmail.com>
2025-09-17 14:13:21 +0000

    finish new items

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,18 @@
+class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to '/admin/items/new'
+  end
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :intorduction, :price, :is_active, :image)
+  end
+end
