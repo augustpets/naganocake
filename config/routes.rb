edfafc73e59e69660f422e8b387fc1e2afd083c5
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-09-17 14:27:06 +0000

    [cart]first

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
+  
+  
   root "homes#top"
   get "about", to: "homes#about", as: "about"
+
+
+  scope module: :public do
+    resources :cart_items, only: [:index, :create, :destroy] do
+      delete :destroy_all, on: :collection
+    end
+  end
 
 
   devise_for :customers,skip: [:passwords], controllers: {
