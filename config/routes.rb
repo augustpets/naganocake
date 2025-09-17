c85a002b100010fc1bbcb4f41a5e85caa3a21385
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-09-17 09:17:23 +0000

    created_new_page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
 
+  namespace :admin do
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
