163c8488aa38b98c37fe1e8dc2d840e779ff6475
Ubuntu <ubuntu@ip-172-31-13-65.ap-northeast-1.compute.internal>
2025-09-18 04:56:13 +0000

    顧客編集ページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,4 +3,12 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def confirm
+  end
+
 end
