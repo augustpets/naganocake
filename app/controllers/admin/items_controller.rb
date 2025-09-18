541eb745962e4f0e51957b59761cf25b809f0cc5
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-09-18 10:55:34 +0000

    編集ページ作成完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,14 +5,38 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to '/admin/items/new'
+    if @item.save
+      redirect_to '/admin/items'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.page(params[:page])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item =Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
 
 
   private
   def item_params
-    params.require(:item).permit(:name, :intorduction, :price, :is_active, :image)
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :genre_id, :image)
   end
 end
