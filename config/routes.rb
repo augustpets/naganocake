ff62d77f02af24107cc164119aecf8a79a3b6c72
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-09-19 08:21:30 +0000

    finish_genre

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
