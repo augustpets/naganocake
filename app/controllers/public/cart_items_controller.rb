ad3ea1aebdbfa6838d4b675567958750eb10a3ae
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-09-19 01:24:08 +0000

    [cart]6

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,7 +4,9 @@ class Public::CartItemsController < ApplicationController
 
   def update
     @cart_item = current_customer.cart_item.find(params[:id])
-    @cart_item
+    @cart_item.update(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @total_amount = @cart_item.inject(0) { |sum, item| sum = item.subtotal }
   end
 
   def destroy
@@ -20,7 +22,7 @@ class Public::CartItemsController < ApplicationController
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
   
 end
