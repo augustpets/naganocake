ac08cc6ae63430b13d7773972881240e2b413c4f
Ubuntu <ubuntu@ip-172-31-13-65.ap-northeast-1.compute.internal>
2025-09-21 04:32:53 +0000

    [管理者側]顧客一覧ページ上の顧客詳細ページリンク作成、ログイン後のリダイレクトパス作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first).create
+Admin.create!(
+  email: 'admin@example.com',
+  password: 'adminpass',
+  password_confirmation: 'adminpass'
+)
