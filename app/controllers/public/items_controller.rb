0adc5038c82d443fd30327b159e5b18e281218a7
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-09-22 06:14:53 +0000

    [Add]pull

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,8 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(8)
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2025_09_19_073925) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
