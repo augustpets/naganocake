b48c8ba896d42a97ccadc6151df726c219f1aefd
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-09-24 16:35:35 +0000

    [order]admin,public

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,7 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 
-  enum status: { waiting: 0, paid_up: 1, production: 2, preparing: 3, shpped: 4 }
+  enum status: { waiting: 0, paid_up: 1, production: 2, preparing: 3, shipped: 4 }
 
   after_update :update_making_status, if: :saved_change_to_status?
   
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
     root :to =>'homes#top'
 
-    get 'orders/show'
+    resources :orders, only: [:index, :show, :update] do
+      resources :order_details, only: [:update]
+    end
   end
 
   namespace :public do
