4c2b414d878246d8f90c3ddc6cf489ceb736c159
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-09-25 02:23:31 +0000

    [status]comp

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,9 +1,10 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     @order = Order.find(params[:order_id])
-    @order_detail = @order.order_detail.find(params[:id])
+    @order_detail = @order.order_details.find(params[:id])
 
-    if @order_detail.update(making_status: params[:order_detail][:making_status])
+    if @order_detail.update(order_detail_params)
+      update_order_status(@order)
       redirect_to admin_order_path(@order), notice: "製作ステータスを更新しました"
     else
       redirect_to admin_order_path(@order), alert: "更新に失敗しました"
@@ -16,4 +17,13 @@ class Admin::OrderDetailsController < ApplicationController
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
   end
+
+  def update_order_status(order)
+    if order.order_details.complete.count == order.order_details.count
+      order.update(status: "preparing")
+    elsif order.order_details.in_production.exists?
+      order.update(status: "production")
+    end
+  end
+
 end
